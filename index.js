dbe9ec11e2ecf78da7330a4abdc8bce35859c2c3
sayuaradark-pixel <sayuaradark@gmail.com>
2026-01-20 22:57:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,28 +2,19 @@ const express = require('express');
 const app = express();
 __path = process.cwd()
 const bodyParser = require("body-parser");
-const PORT = process.env.PORT || 8000;
-let server = require('./qr'),
-    code = require('./pair');
+const PORT = process.env.PORT || 5000;
+let code = require('./pair');
 require('events').EventEmitter.defaultMaxListeners = 500;
-app.use('/server', server);
 app.use('/code', code);
-app.use('/pair',async (req, res, next) => {
+
+app.use('/',async (req, res, next) => {
 res.sendFile(__path + '/pair.html')
 })
-app.use('/qr',async (req, res, next) => {
-res.sendFile(__path + '/qr.html')
-})
-app.use('/',async (req, res, next) => {
-res.sendFile(__path + '/main.html')
-})
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.listen(PORT, () => {
-    console.log(`
-Don't Forgot To Give Star
-
- Server running on http://localhost:` + PORT)
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`⏩ Server running on http://0.0.0.0:` + PORT)
 })
 
 module.exports = app
